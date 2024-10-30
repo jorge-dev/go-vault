a384639d5866674added3cc5ce530922f862f265
jorge-dev <jo.luisa94@gmail.com>
2024-10-30 13:43:44 -0600

    Enhance API server with account management functionality and JSON response handling

--- api.go
+++ api.go
@@ -8,6 +8,7 @@ import (
 
 type ApiServer struct {
 	listenAddr string
+	Store      Storage
 }
 
 type ApiError struct {
@@ -16,27 +17,15 @@ type ApiError struct {
 
 type APIFunc func(w http.ResponseWriter, r *http.Request) error
 
-func NewApiServer(listenAddr string) *ApiServer {
-	return &ApiServer{listenAddr: listenAddr}
-}
-
-func WriteJSON(w http.ResponseWriter, status int, data any) error {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	return json.NewEncoder(w).Encode(data)
-}
-
-func makeHttpHandler(fn APIFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if err := fn(w, r); err != nil {
-			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
-		}
-	}
+func NewApiServer(listenAddr string, store Storage) *ApiServer {
+	return &ApiServer{listenAddr: listenAddr, Store: store}
 }
 
 func (s *ApiServer) Start() {
 	router := http.NewServeMux()
-	router.HandleFunc("GET /account", makeHttpHandler(s.handleGetAccount))
+	router.HandleFunc("GET /account", makeHttpHandler(s.handleGetAccounts))
+	router.HandleFunc("POST /account", makeHttpHandler(s.handleCreateAccount))
+	router.HandleFunc("GET /account/{id}", makeHttpHandler(s.handleGetAccountById))
 
 	log.Println("Starting server on", s.listenAddr)
 
@@ -49,15 +38,39 @@ func (s *ApiServer) Start() {
 	server.ListenAndServe()
 }
 
-func (s *ApiServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+func (s *ApiServer) handleGetAccounts(w http.ResponseWriter, r *http.Request) error {
 	// handle account
-	account := NewAccount(1, "John", "Doe", "1234567890", 100.0)
-	return WriteJSON(w, http.StatusOK, account)
+	accounts, err := s.Store.GetAccounts()
+	if err != nil {
+		log.Default().Println("Error getting accounts hander:", err)
+		return err
+	}
+	return writeJSON(w, http.StatusOK, accounts)
+}
+
+func (s *ApiServer) handleGetAccountById(w http.ResponseWriter, r *http.Request) error {
+	id := r.PathValue("id")
+	log.Println("id:", id)
+	// handle account
+	return writeJSON(w, http.StatusOK, &Account{})
 }
 
 func (s *ApiServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
 	// handle account
-	return nil
+	createAcctReq := new(CreateAccountRequest)
+	if err := json.NewDecoder(r.Body).Decode(createAcctReq); err != nil {
+		log.Default().Println("Error decoding request:", err)
+		return err
+	}
+
+	account := NewAccount(createAcctReq.FirstName, createAcctReq.LastName, createAcctReq.Balance)
+
+	if err := s.Store.CreateAccount(account); err != nil {
+		log.Default().Println("Error creating account:", err)
+		return err
+	}
+
+	return writeJSON(w, http.StatusOK, account)
 }
 
 func (s *ApiServer) handleUpdateAccount(w http.ResponseWriter, r *http.Request) error {
@@ -73,4 +86,18 @@ func (s *ApiServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) 
 func (s *ApiServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
 	// handle account
 	return nil
+}
+
+func writeJSON(w http.ResponseWriter, status int, data any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(data)
+}
+
+func makeHttpHandler(fn APIFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := fn(w, r); err != nil {
+			writeJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
 }
